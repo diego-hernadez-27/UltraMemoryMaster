dc95c38e6b389294b39cd7a6a1d6c34d269029fd
diego-hernadez-27 <dhernandezr1906@alumno.ipn.mx>
2022-06-03 12:57:03 -0500

    conectado a vercel 3.0

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const { MEMORY_APP_MONGODB_HOST ,MEMORY_APP_MONGODB_DATABASE } = process.env;
-const MONGODB_URI = `mongodb://${MEMORY_APP_MONGODB_HOST}/${MEMORY_APP_MONGODB_DATABASE}`;
+const MONGODB_URI = `mongodb+srv://${MEMORY_APP_MONGODB_HOST}/${MEMORY_APP_MONGODB_DATABASE}`;
 
 mongoose.connect(MONGODB_URI, {
   useUnifiedTopology: true,
